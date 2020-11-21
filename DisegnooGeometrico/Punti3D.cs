e0bfc936bee449b0dd8b8d52aa92796394d455af
mark maglio <markmaglio25@gmail.com>
2020-11-21 20:25:26 +0100

    Aggiungere i file di progetto.

---
+++ DisegnooGeometrico/Punti3D.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DisegnooGeometrico
+{
+    class Punti3D
+    {
+        public int z { get; private set; }
+        public Punti3D(int x, int y, int z) : base(x,y)
+        {
+            this.z = z;
+        }
+    }
+}
